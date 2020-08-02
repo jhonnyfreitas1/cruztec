e3283e47b4ac9a26f3b05b15c46961494f98f769
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2020-08-02 11:01:16 -0300

    working response code 200 with offiline

--- sw.js
+++ sw.js
@@ -30,36 +30,12 @@ self.addEventListener('activate', function activator(event) {
     })
   );
 });
-
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (cachedResponse) {
-      if(cachedResponse){
-        return cachedResponse;
-      } 
-      var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(cache_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-              console.log(`algo`)
-            return response;
-          }
-        );
-      })
-    );
-    });
+self.addEventListener('fetch', function(event) {
+  event.respondWith(async function() {
+      try {
+        return await fetch(event.request);
+      } catch (err) {
+          return caches.match('/offline');
+      }
+    }());
+});
